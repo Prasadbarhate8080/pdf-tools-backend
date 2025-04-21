0553635b2f26ef9dbe3892b64d04ca9fc18b08bd
prasadbarhate8080 <prasadbarhate388@gmail.com>
2025-04-21 12:22:45 -0700

    add localhost origin

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ import express from "express"
 import cors from 'cors'
 const app = express();
 import { apiLimiter } from "./middleware/rateLimit.js";
-app.use(cors(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
-))
+const allowedOrigins  = process.env.CORS_ORIGIN?.split(",") || [];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  };
+
+app.use(cors(corsOptions))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
